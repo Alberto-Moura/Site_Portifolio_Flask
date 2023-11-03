6d273b0bcdd55b4d3aa9610eafcec7cb5e74b156
Alberto <albertofmoura@yahoo.com.br>
2023-11-02 21:33:55 -0300

    configuração de animação JS

--- static/script.js
+++ static/script.js
@@ -21,3 +21,24 @@ navItem.forEach(item => {
         }
     })
 })
+
+/* Animar todos os intes na tela que tiverem o atributo data-anime*/
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.scrollY + window.innerHeight * 0.85;
+    
+    item.forEach((Element) => {
+        if (windowTop > Element.offsetTop) {
+            Element.classList.add("animate");
+        } else {
+            Element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
